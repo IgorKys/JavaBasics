f4abad7b5d6598ec83e87127218c19834e3a38df
IgorKysil <igork2210@gmail.com>
2020-07-16 19:07:33 -0700

    Super Call, Method Overloading

---
+++ src/com/syntax/class23/SuperClass.java
@@ -0,0 +1,32 @@
+package com.syntax.class23;
+
+import javax.sql.rowset.spi.SyncFactory;
+
+public class SuperClass {
+
+	String name = "Kamila";
+
+	public void printHello() {
+		System.out.println("Hello from parent class");
+	}
+}
+
+class SubClass extends SuperClass {
+
+	String name = "Mina";
+
+	public void displayName() {
+		System.out.println(this.name);
+		System.out.println(super.name);
+	}
+
+	public void printHello() {
+		System.out.println("Hello from child class");
+	}
+
+	public void callingParentMethod() {
+		// printHello();//this.printHello();
+		super.printHello();
+	}
+
+}
